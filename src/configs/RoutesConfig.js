c4c9b4e53e140a272ec37c5aaf2e1e41f7bb7bc5
@irshadpukayoor@gmail.com <irshadpukayoor@gmail.com>
2024-11-18 18:59:09 +0530

    OCA-38  Arranged Folder Structure

---
+++ src/configs/RoutesConfig.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { AUTH_PREFIX_PATH, APP_PREFIX_PATH } from "configs/AppConfig";
+
+export const publicRoutes = [
+  {
+    key: "login",
+    path: `${AUTH_PREFIX_PATH}/login`,
+    component: React.lazy(() =>
+      import("views/auth-views/authentication/login")
+    ),
+  },
+  {
+    key: "register",
+    path: `${AUTH_PREFIX_PATH}/register`,
+    component: React.lazy(() =>
+      import("views/auth-views/authentication/register")
+    ),
+  },
+  {
+    key: "forgot-password",
+    path: `${AUTH_PREFIX_PATH}/forgot-password`,
+    component: React.lazy(() =>
+      import("views/auth-views/authentication/forgot-password")
+    ),
+  },
+];
+
+export const protectedRoutes = [
+  {
+    key: "dashboard.default",
+    path: `${APP_PREFIX_PATH}/dashboards/default`,
+    component: React.lazy(() => import("views/app-views/dashboards/default")),
+  },
+  {
+    key: "dashboard.analytic",
+    path: `${APP_PREFIX_PATH}/dashboards/analytic`,
+    component: React.lazy(() => import("views/app-views/dashboards/analytic")),
+  },
+  {
+    key: "dashboard.sales",
+    path: `${APP_PREFIX_PATH}/dashboards/sales`,
+    component: React.lazy(() => import("views/app-views/dashboards/sales")),
+  },
+  {
+    key: "event.create",
+    path: `${APP_PREFIX_PATH}/event/create`,
+    component: React.lazy(() => import("views/app-views/event/create-event")),
+  },
+  {
+    key: "event.list",
+    path: `${APP_PREFIX_PATH}/event/list`,
+    component: React.lazy(() => import("views/app-views/event/event-list")),
+  },
+  {
+    key: "event.list",
+    path: `${APP_PREFIX_PATH}/event/list`,
+    component: React.lazy(() => import("views/app-views/event/event-list")),
+  },
+];
---
+++ src/views/app-views/event/create-event/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import EventForm from '../CreateForm';
+
+const AddEvent = () => {
+	return (
+		<EventForm mode="ADD"/>
+	)
+}
+
+export default AddEvent
