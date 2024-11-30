025b359ad930af6eea5c2325eff1af085617e1b8
@irshadpukayoor@gmail.com <irshadpukayoor@gmail.com>
2024-11-30 14:39:13 +0530

    OCA-67 feat: call api from constants

--- src/constants/ApiConstant.js
+++ src/constants/ApiConstant.js
@@ -1,1 +1,3 @@
 export const API_BASE_URL = 'https://uat-tickets2me.mitetechnology.com'
+export const CATEGORY_URL = "/api/v1/events/secured/category"
+export const SUB_CATEGORY_URL = "/api/v1/events/secured/subcategory?category_id="
--- src/services/CategoryService.js
+++ src/services/CategoryService.js
@@ -1,30 +1,31 @@
 import fetch from "auth/FetchInterceptor";
+import {  CATEGORY_URL, SUB_CATEGORY_URL } from "constants/ApiConstant";
 
 const CategoryService = {};
 
 CategoryService.addCategory = function (data) {
   return fetch({
-    url: "/api/v1/events/secured/category",
+    url: CATEGORY_URL,
     method: "post",
     data: data,
   });
 };
 CategoryService.fetchCategory = function () {
   return fetch({
-    url: "/api/v1/events/secured/category",
+    url: CATEGORY_URL,
     method: "get",
   });
 };
 CategoryService.fetchSubCategory = function (categoryId) {
   return fetch({
-    url: `/api/v1/events/secured/subcategory?category_id=${categoryId}`,
+    url: `${SUB_CATEGORY_URL}${categoryId}`,
     method: "get",
   });
 };
 
 CategoryService.addSubCategory = function (data, categoryId) {
   return fetch({
-    url: `/api/v1/events/secured/subcategory?category_id=${categoryId}`,
+    url: `${SUB_CATEGORY_URL}${categoryId}`,
     method: "post",
     data: data,
   });
