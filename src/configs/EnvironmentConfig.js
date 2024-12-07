ba934429491a2d8be4945eb7074d457393df5be9
@irshadpukayoor@gmail.com <irshadpukayoor@gmail.com>
2024-12-07 10:26:25 +0530

    url updated

--- src/configs/EnvironmentConfig.js
+++ src/configs/EnvironmentConfig.js
@@ -1,9 +1,9 @@
 const dev = {
   API_ENDPOINT_URL: 'https://uat-tickets2me.mitetechnology.in'
-//   API_ENDPOINT_URL: 'http://192.168.1.21:8000'
+//   API_ENDPOINT_URL: 'http://192.168.1.100:8000'
 };
 const prod = {
-	API_ENDPOINT_URL: '/api'
+	API_ENDPOINT_URL: 'https://uat-tickets2me.mitetechnology.in'
 };
 
 const test = {
