af71448f701f87c7ce7466d5c8d78cedd0b798a1
shamil yusuf <shamil707@gmail.com>
2025-01-25 04:27:31 +0000

    EnvironmentConfig.js edited online with Bitbucket

--- src/configs/EnvironmentConfig.js
+++ src/configs/EnvironmentConfig.js
@@ -1,37 +1,40 @@
-const envConfigs = {
-  development: {
-    API_ENDPOINT_URL: "http://127.0.0.1:8000",
-  },
-  demo: {
-    API_ENDPOINT_URL: "https://demo-tickets2me.mitetechnology.in",
-  },
-  uat: {
-    API_ENDPOINT_URL: "https://uat-tickets2me.mitetechnology.in",
-  },
-  production: {
-    API_ENDPOINT_URL: "https://tickets2me.mitetechnology.in",
-  },
-  test: {
-    API_ENDPOINT_URL: "/api",
-  },
-} as const;
-
-// Define the possible keys as a union type
-type EnvKeys = keyof typeof envConfigs;
-
-// Type guard to check if the environment is valid
-const getEnvKey = (env: string | undefined): EnvKeys => {
-  if (
-    env === "development" ||
-    env === "demo" ||
-    env === "uat" ||
-    env === "production" ||
-    env === "test"
-  ) {
-    return env;
-  }
-  return "development"; // Default to development if undefined or invalid
+const dev = {
+  API_ENDPOINT_URL: "http://127.0.0.1:8000",
 };
 
-// Use the type-guarded environment variable
-export const env = envConfigs[getEnvKey(process.env.NEXT_PUBLIC_ENV)];
+const demo = {
+  API_ENDPOINT_URL: "https://demo-tickets2me.mitetechnology.in",
+};
+
+const uat = {
+  API_ENDPOINT_URL: "https://uat-tickets2me.mitetechnology.in",
+};
+
+const prod = {
+  API_ENDPOINT_URL: "https://tickets2me.mitetechnology.in",
+};
+
+const test = {
+  API_ENDPOINT_URL: "/api",
+};
+
+
+const getEnv = () => {
+  switch (process.env.REACT_APP_ENV) {
+    case "demo":
+      return demo;
+    case "uat":
+      return uat;
+    case "production":
+      return prod;
+    case "development":
+      return dev;
+    case "test":
+      return test;
+    default:
+      return dev;  // Fallback to development
+  }
+};
+
+export const env = getEnv();
+
