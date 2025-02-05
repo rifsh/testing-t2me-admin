0bce2a50a9fae7728f940babb80b3ff8a99ea32e
shaheenismail <shaheen@mitesolutions.com>
2025-02-05 13:42:45 +0530

    OCA-216 setting up faq admin front end

--- src/views/app-managment/layout/faq/list-faq/index.js
+++ src/views/app-managment/layout/faq/list-faq/index.js
@@ -1,61 +1,112 @@
 import React from 'react';
-import { FormOutlined } from '@ant-design/icons';
-import {Card, Col, Button, Table} from "antd";
+import { FormOutlined, EditOutlined, DeleteOutlined } from '@ant-design/icons';
+import { Card, Col, Button, Table, Menu } from 'antd';
 import { useNavigate } from 'react-router-dom';
 import { APP_PREFIX_PATH } from 'configs/AppConfig';
-import { faqData } from 'mock/data/faqData'
-import SearchBarWithStatus from "components/util-components/Search/SearchBarWithStatus"; 
+import { faqData } from 'mock/data/faqData';
+import SearchBarWithStatus from 'components/util-components/Search/SearchBarWithStatus';
+import EllipsisDropdown from "components/shared-components/EllipsisDropdown";
+import Flex from "components/shared-components/Flex";
 
-const { Row } = require("antd")
-
-
+const { Row } = require('antd');
 
 const FaqList = () => {
-    const navigate =useNavigate();
-    const tableCloumns = [
+    const navigate = useNavigate();
+
+    const groupedData = faqData.reduce((acc, item) => {
+        if (!acc[item.category]) {
+            acc[item.category] = [];
+        }
+        acc[item.category].push(item);
+        return acc;
+    }, {});
+
+    const dataSource = Object.keys(groupedData).map((category) => ({
+        key: category,
+        category,
+        questions: groupedData[category],
+    }));
+
+    const dropdownMenu = (row) => (
+        <Menu>
+          <Menu.Item>
+            <Flex alignItems="center" >
+              <EditOutlined />
+              <span className="ml-2">Edit Faq</span>
+            </Flex>
+          </Menu.Item>
+          <Menu.Item>
+            <Flex alignItems="center" >
+              <DeleteOutlined />
+              <span className="ml-2">Delete Faq</span>
+            </Flex>
+          </Menu.Item>
+        </Menu>
+      );
+
+    const categoryColumns = [
         {
-            title: "Category",
-            dataIndex: "category",
-            key: "question",
-            width: '30%'
+            title: 'Category',
+            dataIndex: 'category',
+            key: 'category',
+        },
+    ];
+
+    const questionColumns = [
+        {
+            title: '',
+            dataIndex: 'question',
+            key: 'question',
         },
         {
-            title: "Questions",
-            dataIndex: "question",
-            key: "question",
-            width: '70%'
-        }
+              title: "",
+              dataIndex: "actions",
+              render: (_, elm) => (
+                <div className="text-right">
+                  <EllipsisDropdown menu={dropdownMenu(elm)} />
+                </div>
+              ),
+            },
     ];
 
     return (
         <Card>
-            <Row gutter={16} justify = {"space-between"} style={{marginBottom: 16}}>
-            <SearchBarWithStatus />
-                <Col xs={24} sm={8} style={{textAlign: "right"}}>
-                <Button
-                type="primary"
-                icon={<FormOutlined/>}
-                onClick={() => navigate(`${APP_PREFIX_PATH}/app/management/layout/faq/add-faq`)}
-                >
-                    Add Faq
-                </Button>
+            <Row gutter={16} justify={'space-between'} style={{ marginBottom: 16 }}>
+                <SearchBarWithStatus />
+                <Col xs={24} sm={8} style={{ textAlign: 'right' }}>
+                    <Button
+                        type="primary"
+                        icon={<FormOutlined />}
+                        onClick={() => navigate(`${APP_PREFIX_PATH}/app/management/layout/faq/add-faq`)}
+                    >
+                        Add Faq
+                    </Button>
                 </Col>
             </Row>
             <div className="table-responsive">
                 <Table
-                columns={tableCloumns}
-                dataSource={faqData}
-                expandable={{
-                    expandedRowRender: (record) => (
-                        <p style={{ margin: 0 }}>{record.answer}</p>
-                    ),
-                    rowExpandable: (record) => record.answer != 'No Replies',
-                
-                }}
+                    columns={categoryColumns}
+                    dataSource={dataSource}
+                    expandable={{
+                        expandedRowRender: (record) => (
+                            <Table
+                                columns={questionColumns}
+                                dataSource={record.questions}
+                                pagination={false}
+                                expandable={{
+                                    expandedRowRender: (questionRecord) => (
+                                        <p style={{ margin: 0 }}>{questionRecord.answer}</p>
+                                    ),
+                                    rowExpandable: (questionRecord) => questionRecord.answer !== 'No Replies',
+                                }}
+                            />
+                        ),
+                        rowExpandable: (record) => record.questions.length > 0,
+                    }}
                 />
             </div>
         </Card>
-    )
+    );
 };
 
 export default FaqList;
