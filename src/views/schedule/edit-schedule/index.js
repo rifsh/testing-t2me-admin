5ce4d4bce7bb270b6e05cdbf66e061b424bfa350
@irshadpukayoor@gmail.com <irshadpukayoor@gmail.com>
2025-02-07 14:23:52 +0530

    updated schedule details

---
+++ src/views/schedule/edit-schedule/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ScheduleForm from "../form-schedule/MultyStepScheduleForm";
+
+const EditSchedule = () => {
+  return <ScheduleForm mode="EDIT" />;
+};
+
+export default EditSchedule;
