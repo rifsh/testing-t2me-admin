4da0762214d9b4a0da6ea4602afff06b8b4f0d33
@irshadpukayoor@gmail.com <irshadpukayoor@gmail.com>
2025-02-21 19:04:31 +0530

    handled normal ticket structure case

--- src/store/slices/modalSlice.js
+++ src/store/slices/modalSlice.js
@@ -20,7 +20,6 @@ const modalSlice = createSlice({
       state.selectedItem = action.payload;
     },
     setSelectedSubmitItem: (state, action) => {
-      console.log(action.payload, "ticketData");
       state.selectedSubmitItem = action.payload;
     },
     setDialogVisible: (state, action) => {
