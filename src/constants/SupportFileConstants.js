efe29a4518225f55775db8c1dcc09e39944b4248
@irshadpukayoor@gmail.com <irshadpukayoor@gmail.com>
2025-02-28 15:57:14 +0530

    update image upload

--- src/constants/SupportFileConstants.js
+++ src/constants/SupportFileConstants.js
@@ -53,8 +53,10 @@ export const parseSizeToBytes = (size) => {
 
 export const ThumbnailImageResolutions = {
   PLACE: { width: 1080, height: 720 },
+  VENUE: { width: 1080, height: 720 },
   EVENT_BANNER: { width: 2000, height: 720 },
   EVENT: { width: 1080, height: 720 },
+  CATEGORY: { width: 1080, height: 720 },
   OFFER: { width: 1080, height: 1080 },
   COUPON: { width: 1080, height: 1080 },
 };
