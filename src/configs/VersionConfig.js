52007cfcd6591d7679d481a38b31bf650d6a54a6
shamil707 <shamil707@gmail.com>
2025-03-06 15:05:33 +0800

    Added the data

--- src/configs/VersionConfig.js
+++ src/configs/VersionConfig.js
@@ -1,2 +1,3 @@
 export const APP_CURRENT_VERSION = "v-1.3.6";
 export const BUILD_TIMESTAMP = process.env.REACT_APP_BUILD_TIMESTAMP || "Unknown";
+export const BUILD_COMMIT = process.env.REACT_APP_COMMIT_HASH || "Unknown";
