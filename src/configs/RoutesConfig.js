29d66509c78b8784a2ca847046aedf869ec624a6
@irshadpukayoor@gmail.com <irshadpukayoor@gmail.com>
2025-04-14 12:53:00 +0530

    TIC-246 Admin side - schedule(movie) - 1st screen for selecting place, venue, other details

--- src/configs/RoutesConfig.js
+++ src/configs/RoutesConfig.js
@@ -169,6 +169,11 @@ export const protectedRoutes = [
     component: React.lazy(() => import("views/Movie/Schedule/schedule-list")),
   },
   {
+    key: "movie.schedule",
+    path: `${APP_PREFIX_PATH}/movie-schedule/add`,
+    component: React.lazy(() => import("views/Movie/Schedule/schedule-add")),
+  },
+  {
     key: "movie.theater",
     path: `${APP_PREFIX_PATH}/movie-theater/list`,
     component: React.lazy(() => import("views/theater/list-theater")),
