16cf4069a1478878d8d11256e3c9de5fad1e2473
nihal <nihal@mitesolutions.com>
2025-05-29 12:01:18 +0530

    enc off

--- src/configs/EnvironmentConfig.js
+++ src/configs/EnvironmentConfig.js
@@ -5,16 +5,16 @@ const dev = {
   // API_ENDPOINT_URL: "https://uat-tickets2me.mitetechnology.in",
    API_ENDPOINT_URL: "http://127.0.0.1:8000",
   AES_KEY: "your_32_byte_encryption_key_here", // 32 characters
-  NEED_ENCRYPT_DECRYPT: true, 
-  ENCRYPT_PARAMS: true,
+  NEED_ENCRYPT_DECRYPT: false, 
+  ENCRYPT_PARAMS: false,
   SKIP_ENCRYPTION_PATHS: COMMON_SKIP_ENCRYPTION_PATHS
 };
 
 const uat = {
   API_ENDPOINT_URL: "https://uat-tickets2me.mitetechnology.in",
   AES_KEY: "your_32_byte_encryption_key_here", // 32 characters
-  NEED_ENCRYPT_DECRYPT: true, 
-  ENCRYPT_PARAMS: true,
+  NEED_ENCRYPT_DECRYPT: false, 
+  ENCRYPT_PARAMS: false,
   SKIP_ENCRYPTION_PATHS: COMMON_SKIP_ENCRYPTION_PATHS
 };
 const demo = {
